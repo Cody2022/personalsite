31fda4082c7b7b941f863bacd7937f007c502412
Cody2022 <cody.zheng@hotmail.com>
2022-08-04 23:33:39 -0600

    Added different routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+// import Navbar from "./Components/Navbar";
+import Navbar from "./Components/Navbar";
+import { Route, Routes } from "react-router-dom";
+import SoftwareEngineer from "./Pages/SoftwareEngineer";
+import PetroleumEngineer from "./Pages/PetroleumEngineer";
+import Resume from "./Pages/Resume";
+import Interests from "./Pages/Interests";
+import Footer from "./Components/Footer";
+
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="navbar">
+        <Navbar />
       </header>
+      <div>
+        <Routes>
+          <Route path="/" element={<SoftwareEngineer />} />
+          <Route path="/softwareengineer" element={<SoftwareEngineer />} />
+          <Route path="/petroleumengineer" element={<PetroleumEngineer />} />
+          <Route path="/interests" element={<Interests />} />
+          <Route path="/resume" element={<Resume />} />
+        </Routes>
+      </div>
+      <Footer/>
     </div>
   );
 }
